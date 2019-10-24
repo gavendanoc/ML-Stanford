d4c48fc97b6f675839765ce2f4a264b6ce93718d
Gabriel <gabriel19oct@hotmail.com>
2019-10-24 08:42:36 -0500

    week8 problem ser aproved! :see_no_evil:

--- machine-learning-ex7/machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/machine-learning-ex7/ex7/computeCentroids.m
@@ -26,7 +26,11 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
+for k=1:K
+    selectedPoints = X(idx==k, :);
+    selectedPointsSize = size(selectedPoints, 1);
+    centroids(k, :) = sum(selectedPoints) ./ selectedPointsSize;
+end
 
 
 
